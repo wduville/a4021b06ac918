7ca202b9365159bbb57b466af14f1e3281cc1dcc
wduville <willy.duville@gmail.com>
2022-01-25 16:06:33 +0100

    refact(dag_pipeline_exercise): Move code to "dags-airflow/src" sub folder

--- dags-airflow/dag_pipeline_exercice.py
+++ dags-airflow/dag_pipeline_exercice.py
@@ -3,14 +3,12 @@ from datetime import timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
-from airflow.utils.dates import days_ago, datetime
-
+from airflow.utils.dates import days_ago
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'start_date': days_ago(1),
-    #'start_date': datetime(2020, 11, 10),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -37,28 +35,28 @@ start = DummyOperator(task_id='start', dag=dag)
 start.__doc__ = "Stat Operator"
 
 
-from fetch_all_datasets import fetch_all_datasets
+from src.fetch_all_datasets import fetch_all_datasets
 fetch_all = PythonOperator(
     task_id='fetch_all_datasets',
     python_callable=fetch_all_datasets,
     dag=dag,
 )
 
-from merge_and_filter_datasets import merge_and_filter_datasets
+from src.merge_and_filter_datasets import merge_and_filter_datasets
 merge_and_filter = PythonOperator(
     task_id='merge_and_filter_datasets',
     python_callable=merge_and_filter_datasets,
     dag=dag,
 )
 
-from export_to_csv import export_to_csv
+from src.export_to_csv import export_to_csv
 export_to_csvs = PythonOperator(
     task_id='export_to_csv',
     python_callable=export_to_csv,
     dag=dag,
 )
 
-from fetch_all_organizations import fetch_all_push_mongo
+from src.fetch_all_organizations import fetch_all_push_mongo
 filter_and_push_mongo = PythonOperator(
     task_id='filter_and_push_mongo',
     python_callable=fetch_all_push_mongo,
---
+++ dags-airflow/src/fetch_all_datasets.py
@@ -0,0 +1,38 @@
+import requests
+import json
+import pathlib
+
+from src.tools import Timer
+
+
+def fetch_all_datasets():
+    page_size = 4000
+    x_fields = 'total,page,next_page,previous_page,uri,page_size,data{id,title,slug,metrics,organization,page,description,tags}'
+
+    result = run_query({'page': 0, 'page_size': 0}, headers={'X-Fields': 'total'})
+    print("Total:", result['total'])
+
+    with Timer("main"):
+        for page in range(1, (result['total'] // 4000) + 1):
+            with Timer(f"Page {page}"):
+                result = run_query({'page': page, 'page_size': page_size}, headers={'X-Fields': x_fields})
+                datasets = result['data']
+                print(len(datasets), "items")
+
+            filename = pathlib.Path(f'data/datasets_page_{page:02d}_size_{page_size}.json')
+            filename.parent.mkdir(exist_ok=True)
+            with filename.open('w') as f:
+                json.dump(obj=result, fp=f, indent=2)
+
+        print(len(datasets))
+
+
+def run_query(params: dict = None, headers: dict = None, base_url: str = "https://www.data.gouv.fr/api/1/datasets/"):
+    request = requests.get(url=base_url, params=params, headers=headers)
+    if request.status_code == 200:
+        return request.json()
+    raise Exception(f"Query failed to run by returning code of {request.status_code}: {request.reason}. {params}")
+
+
+if __name__ == '__main__':
+    fetch_all_datasets()
